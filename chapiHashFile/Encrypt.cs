4c162ebf0628991e49bb1c871b8454ef22941884
nima <nimachapi1994@gmail.com>
2019-07-16 17:03:16 -1000

    this is cryptography project for use encrypt and decrypt files in local pc if you use this project please remember your password

---
+++ chapiHashFile/Encrypt.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapiHashFile
+{
+    class Encrypt
+    {
+        public void EncryptFile(string file, string password, string output)
+        {
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            Random random = new Random();
+          var rnd=  random.Next(1, 999999);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(1+password+"خورشید سوزناک در حمله هیتلر" + 123456789 + "زنده باد کوروش کبیر");
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            File.WriteAllBytes(output, bytesEncrypted);
+        }
+
+        public void DecryptFile(string file, string password, string output)
+        {
+
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(file);
+            Random random = new Random();
+            var rnd = random.Next(1, 999999);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(1+password + "خورشید سوزناک در حمله هیتلر"+ 123456789 + "زنده باد کوروش کبیر" );
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            File.WriteAllBytes(output, bytesDecrypted);
+        }
+
+        private byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+    }
+}
---
+++ chapiHashFile/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace chapiHashFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        internal static string GetStringSha256Hash(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return String.Empty;
+
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                byte[] textData = System.Text.Encoding.UTF8.GetBytes(text);
+                byte[] hash = sha.ComputeHash(textData);
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+           
+           
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string filepath = openFileDialog1.FileName;
+                Encrypt d = new Encrypt();
+                d.DecryptFile(filepath, textBox2.Text, filepath);
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox=false;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //hash all files
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                string filepath = openFileDialog1.FileName;
+                Encrypt d = new Encrypt();
+                d.EncryptFile(openFileDialog1.FileName, textBox1.Text, openFileDialog1.FileName);
+                //string filetext = "hi hasan khrdal sose kachab amin  nima aragh " + Environment.NewLine + "nima" + Environment.NewLine + "hasan";
+                //string x = GetStringSha256Hash(filetext);
+                //File.Delete(filepath);
+                //File.WriteAllText(filepath , x, Encoding.UTF8);
+
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            MessageBox.Show(random.Next(1, 999999).ToString());
+        }
+    }
+}
